62e3cc497d4a012a5de0c82dfdc8597386f683f9
soli-exe <sja.exe@gmail.com>
2022-04-16 23:11:24 +0430

    Custom color sheme defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
+        colors: {
+            'transparent': 'transparent',
+            'text-dark': '#27282D',
+            'text-light': '#FFFFFF',
+            'bg-dark': '#0E141C',
+            'bg-light': '#F8F8F8',
+            'box-dark': '#202C3C',
+            'box-light': '#F8F8F8',
+            'purple-dark': '#9633BC',
+            'purple-light': '#9D6FFF',
+            'shadow-dark': '#34343425'
+        }
     },
     plugins: [],
 }
