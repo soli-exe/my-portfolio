2f99597be03162f13bd105fffef9c40091cf9352
soli-exe <sja.exe@gmail.com>
2022-04-16 23:18:49 +0430

    justify containers to center by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
+        container: {
+            center: true,
+        },
         colors: {
             'transparent': 'transparent',
             'text-dark': '#27282D',
