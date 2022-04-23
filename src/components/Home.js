d5992978a6ddd0ee69d7925bdcef07dbaadeeb84
soli-exe <sja.exe@gmail.com>
2022-04-23 12:00:11 +0430

    New component: This component has the task of rendering home page components and elements

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Intro from './Intro';
+import About from './About';
+import Contact from './Contact';
+import styled from 'styled-components';
+
+
+const Divider = styled.hr`
+    border: none;
+    border-bottom: 1px dashed #9633BC;
+    margin: 0 20%;
+`;
+
+const Home = () => {
+    return (
+        <div className='max-w-7xl space-y-8 bg-bg-light'>
+            <Intro />
+            <Divider></Divider>
+            <About />
+            <Divider></Divider>
+            <Contact />
+        </div>
+    );
+};
+
+export default Home;
