acae49c4c032cf85f6a7e077e76ab91e8c18c798
soli-exe <sja.exe@gmail.com>
2022-04-27 13:03:06 +0430

    update: projects component imported
    The router for projects page has been set

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import '../src/tailwind.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/skills' element={<Skills />} />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
     </div>
   );
