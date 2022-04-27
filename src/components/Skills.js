25c70beb0cd7ae5bede23332f81e8e29fcb39661
soli-exe <sja.exe@gmail.com>
2022-04-27 11:13:41 +0430

    new component: Skills component developed
    This component task is to show specializations in general

---
+++ src/components/Skills.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Skills = () => {
+
+    const [data, setData] = useState([]);
+
+    const getData = () => {
+        fetch('data.json')
+            .then(res => {
+                return res.json();
+            })
+            .then(
+                data => {
+                    return setData(data.skills);
+                }
+            )
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+
+    return (
+        <div className='max-w-7xl mt-4 bg-bg-light sm:px-8'>
+            <div className='p-6 w-full drop-shadow-custom bg-box-light sm:rounded-lg space-y-8'>
+                <header className='text-center md:text-left' >
+                    <h3 className='text-2xl font-black text-purple-dark'>
+                        Specializations
+                    </h3>
+                </header>
+                <div className='flex flex-wrap justify-center md:justify-between gap-12'>
+                    {data && data.map(skill => (
+                        <section key={skill.id} className='w-64 md:flex-auto'>
+                            <header className='flex gap-x-2 mb-2'>
+                                <img src={skill.icons[0]} alt={skill.name} />
+                                {skill.icons[1] && <img src={skill.icons[1]} alt={skill.name} />}
+                            </header>
+                            <p className='text-md font-medium text-start'>
+                                {skill.description}
+                            </p>
+                        </section>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
