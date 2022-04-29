0a8414a8e1d7453a473c264d27ee29e4885de755
soli-exe <sja.exe@gmail.com>
2022-04-29 15:51:26 +0430

    update: ProjectCard component imported
    Another style issues fixed which had some bugs with showing project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import ProjectCard from './ProjectCard';
 
 const Container = styled.div`
     display: grid;
+    width: 100%;
+    height: max-content;
     overflow-y: auto;
-    grid-template-columns: repeat(auto-fill, minmax(min(100%, 20rem), 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(min(100%, 25rem), 1fr));
     grid-template-rows: 1fr 1fr 1fr 1fr;
     justify-items: center;
     grid-gap: 1.5rem; 
@@ -26,19 +29,17 @@ const Projects = () => {
 
 
     return (
-        <div className='max-w-7xl w-full mt-4 bg-bg-light sm:px-8'>
-            <div className='w-full bg-box-light sm:rounded-lg space-y-8'>
-                <header className='text-center md:text-left' >
-                    <h3 className='text-2xl font-black text-purple-dark'>
-                        Showcases
-                    </h3>
-                </header>
-                <Container>
-                    {projects && projects.map(project => {
-                        return project
-                    })}
-                </Container>
-            </div>
+        <div className='max-w-7xl w-full mt-4 sm:px-4'>
+            <header className='text-center md:text-left sm:px-4'>
+                <h3 className='text-2xl font-black text-purple-dark'>
+                    Showcases
+                </h3>
+            </header>
+            <Container className='sm:px-4 py-6'>
+                {projects && projects.map(project => {
+                    return <ProjectCard key={project.id} title={project.title} description={project.description} labels={project.labels} icon={project.icon} link={project.link} githubRepoLink={project.githubRepo} />
+                })}
+            </Container>
         </div>
     );
 };
