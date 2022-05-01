e841031a80c8bef1a0e12f50171899e5598a7df5
soli-exe <sja.exe@gmail.com>
2022-05-01 23:24:23 +0430

    update: wrapping all component between themeProvider
    This will pass the theme state through all component tree which provided by ThemeContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import '../src/tailwind.css';
+import { ThemeProvider } from './components/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 import Header from './components/Header';
 import Home from './components/Home';
 import Skills from './components/Skills';
@@ -8,14 +10,16 @@ import Projects from './components/Projects';
 
 const App = () => {
   return (
-    <div className="grid justify-items-center md:px-16 lg:px-32">
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/skills' element={<Skills />} />
-        <Route path='/projects' element={<Projects />} />
-      </Routes>
-    </div>
+    <ThemeProvider>
+      <div className="grid justify-items-center md:px-16 lg:px-32">
+        <Header toggleTheme={<ThemeToggle />} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 };
 
