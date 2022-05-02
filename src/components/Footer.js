346cb888d2fd27a0f0f3fea8d79622a6d044af4a
soli-exe <sja.exe@gmail.com>
2022-05-02 16:14:05 +0430

    remove: unsed assigned variables removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import githubIcon from '../assets/icons/github.svg';
-import instagramIcon from '../assets/icons/instagram.svg';
-
-const Img = styled.img`
-    width: 2rem;
-`;
 
 
 const Footer = () => {
