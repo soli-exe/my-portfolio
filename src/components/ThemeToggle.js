d3454fb7b8c66f99873101dfafcb035e5c8cc961
soli-exe <sja.exe@gmail.com>
2022-05-02 15:03:31 +0430

    style update: using width
    Using width for toggler icon instead of padding for better alignment

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -11,8 +11,8 @@ const ThemeToggle = () => {
     return (
         <div className='transition duration-500 ease-in-out'>
             {theme === 'dark' ?
-                <FontAwesomeIcon onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} icon={faSun} className='text-text-light dark:text-text-light md:text-text-dark p-2' />
-                : <FontAwesomeIcon onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} icon={faMoon} className='text-text-light dark:text-text-light md:text-text-dark p-2' />}
+                <FontAwesomeIcon onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} icon={faSun} className='text-text-light dark:text-text-light md:text-text-dark w-6' />
+                : <FontAwesomeIcon onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} icon={faMoon} className='text-text-light dark:text-text-light md:text-text-dark w-6' />}
         </div>
     );
 };
