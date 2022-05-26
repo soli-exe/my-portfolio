790dfb37ffac607c6adc5d2f4341fda4f077d671
soli-exe <sja.exe@gmail.com>
2022-05-26 20:32:27 +0430

    update: use loading
    Maybe sometime data takes a moment to be set in state so for better ux Im using a loading animation

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+// Components
+import Loading from './Loading';
 
 const Skills = () => {
 
@@ -24,26 +26,31 @@ const Skills = () => {
 
     return (
         <div className='max-w-7xl mt-4 sm:px-8'>
-            <div className='p-6 w-full drop-shadow-custom bg-box-light sm:rounded-lg space-y-8 box-dark-mode'>
-                <header className='text-center md:text-left' >
-                    <h3 className='text-2xl font-black text-purple-dark text-purple-dark-mode'>
-                        Specializations
-                    </h3>
-                </header>
-                <div className='flex flex-wrap justify-center md:justify-between gap-12'>
-                    {data && data.map(skill => (
-                        <section key={skill.id} className='w-64 md:flex-auto'>
-                            <header className='flex gap-x-2 mb-2'>
-                                <img src={skill.icons[0]} alt={skill.name} />
-                                {skill.icons[1] && <img src={skill.icons[1]} alt={skill.name} />}
-                            </header>
-                            <p className='text-md font-medium text-start text-text-dark text-dark-theme'>
-                                {skill.description}
-                            </p>
-                        </section>
-                    ))}
-                </div>
-            </div>
+            {data.length ?
+                <>
+                    <div className='p-6 w-full drop-shadow-custom bg-box-light sm:rounded-lg space-y-8 box-dark-mode'>
+                        <header className='text-center md:text-left' >
+                            <h3 className='text-2xl font-black text-purple-dark text-purple-dark-mode'>
+                                Specializations
+                            </h3>
+                        </header>
+                        <div className='flex flex-wrap justify-center md:justify-between gap-12'>
+                            {data.map(skill => (
+                                <section key={skill.id} className='w-64 md:flex-auto'>
+                                    <header className='flex gap-x-2 mb-2'>
+                                        <img src={skill.icons[0]} alt={skill.name} />
+                                        {skill.icons[1] && <img src={skill.icons[1]} alt={skill.name} />}
+                                    </header>
+                                    <p className='text-md font-medium text-start text-text-dark text-dark-theme'>
+                                        {skill.description}
+                                    </p>
+                                </section>
+                            ))}
+                        </div>
+                    </div>
+                </> :
+                <Loading />
+            }
         </div>
     );
 };
